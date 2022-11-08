09529eed12221a0dcea75e42c3d55a509e79bb5d
Jhonata Santos <jhonata.santos@semantix.ai>
2022-11-07 21:07:40 -0300

    feat: add taskList, modal with edit and remove elements from list

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,45 +1,71 @@
-import React ,{useState, ChangeEvent, FormEvent, useEffect} from 'react'
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react'
 
 import styles from './TaskForm.module.css'
-import {ITask} from '../interface/ITask'
+import { ITask } from '../interface/ITask'
 
 
-interface Props  {
-  btnText: string
+interface Props {
+  btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+  task?: ITask | null;
 }
 
 
 
-const TaskForm = ({btnText}: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList, task }: Props) => {
 
-  const [id, setId] = useState<number>(0)
-  const [title, setTitle] = useState<string>("")
-  const [difficulty, setDifficulty] = useState<number>(0)
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
 
-  const addTaskHandler = () => {
+  useEffect(() => {
+    if(task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task])
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = Math.floor(Math.random() * 1000)
+    const newTask: ITask = { id, title, difficulty }
+    setTaskList!([...taskList, newTask]);
+
+    setTitle("")
+    setDifficulty(0)
 
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      if(e.target.name === 'title') {
-        setTitle(e.target.value)
-      } else {
-        setDifficulty(parseInt(e.target.value))
-      }
-
-      console.log(title)
-      console.log(difficulty)
+    if (e.target.name === 'title') {
+      setTitle(e.target.value)
+    } else {
+      setDifficulty(parseInt(e.target.value))
+    }
   }
 
   return (
     <form onSubmit={addTaskHandler} className={styles.form}>
       <div className={styles.input_container}>
         <label htmlFor="title"> Title: </label>
-        <input type="text" name="title" placeholder='Title of task' onChange={handleChange} />
+        <input
+          type="text"
+          name="title"
+          placeholder='Title of task'
+          onChange={handleChange}
+          value={title} />
       </div>
       <div className={styles.input_container}>
         <label htmlFor="difficult"> Dificuldade: </label>
-        <input type="text" name="title" placeholder='Difficult of task' onChange={handleChange}/>
+        <input
+          type="text"
+          name="difficult"
+          placeholder='Difficult of task'
+          onChange={handleChange}
+          value={difficulty} />
       </div>
       <input type="submit" value={btnText} />
     </form>
