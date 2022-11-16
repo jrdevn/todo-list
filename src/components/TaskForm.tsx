209778147e208bcb2c4dfca2c99886be3506c896
Jhonata Santos <jhonata.santos@semantix.ai>
2022-11-16 18:27:46 -0300

    feat: finalizando o metodo update

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -9,11 +9,12 @@ interface Props {
   taskList: ITask[];
   setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
   task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void;
 }
 
 
 
-const TaskForm = ({ btnText, taskList, setTaskList, task }: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList, task, handleUpdate }: Props) => {
 
   const [id, setId] = useState<number>(0);
   const [title, setTitle] = useState<string>("");
@@ -30,12 +31,16 @@ const TaskForm = ({ btnText, taskList, setTaskList, task }: Props) => {
   const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const id = Math.floor(Math.random() * 1000)
-    const newTask: ITask = { id, title, difficulty }
-    setTaskList!([...taskList, newTask]);
-
-    setTitle("")
-    setDifficulty(0)
+    if (handleUpdate) {
+      handleUpdate(id, title, difficulty)
+    } else {
+      const id = Math.floor(Math.random() * 1000)
+      const newTask: ITask = { id, title, difficulty }
+      setTaskList!([...taskList, newTask]);
+  
+      setTitle("")
+      setDifficulty(0)
+    }
 
   }
 
